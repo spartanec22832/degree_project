1f410c7dca5283eb9c783e98b74ff95441ed99df
spartanec22832 <b.bogovski@yandex.ru>
2026-06-03 17:07:53 +0300

    configure api-url on client

--- degree_android/app/src/main/java/com/sfedu/degree_android/core/network/ApiConstants.kt
+++ degree_android/app/src/main/java/com/sfedu/degree_android/core/network/ApiConstants.kt
@@ -1,6 +1,6 @@
 package com.sfedu.degree_android.core.network
 
 object ApiConstants {
-    const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "https://datum.dedyn.io:444/"
     const val IMAGES_BASE_URL = BASE_URL
 }
